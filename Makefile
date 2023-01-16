0b860c3f7183d63a4221b110cc1076fc6889f9e7
dima_nikitin <nikitindima1206@gmail.com>
2023-01-17 01:13:47 +0200

    connect socketio with front js

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 run:
 	poetry run uvicorn base_api.config.app:app --reload
 
-
 run_sockets:
-    uvicorn sockets.config.app:app --reload --port 8001
+	uvicorn sockets.config.app:app --reload --port 8001
--- sockets/socketio_server.py
+++ sockets/socketio_server.py
@@ -1,11 +1,17 @@
 import socketio
 
 # create a Socket.IO server
-sio = socketio.AsyncServer()
+sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins="*")
 
 
 @sio.event
-def connect_to_chat(sid, environ, auth):
+def connect(sid, environ):
+    print('connect ', sid)
+    print('dima connected')
+
+
+@sio.event
+def connect_to_chat(sid):
     print('connect ', sid)
 
 
